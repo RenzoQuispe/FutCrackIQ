f0812dd1dc976b53b2aed20e8e15a377dc90dfdd
RenzoQuispe <renzo123cd@gmail.com>
2025-09-23 18:17:21 -0500

    homepage inicial

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () =>
+            import('./features/home/home-routing.module').then(m => m.HomeRoutingModule)
+    },
+    { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule { }
---
+++ frontend/src/app/features/home/pages/home-page/home-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  templateUrl: './home-page.component.html',
+})
+export class HomePageComponent {
+  title = 'Bienvenido a FutCracksIQ';
+}
